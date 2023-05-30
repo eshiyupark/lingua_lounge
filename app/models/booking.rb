2e8ed84e76df9a61f125570db08ffe830451b61c
Ayanthi Saparamadu <nadeeshaddna@gmail.com>
2023-05-30 14:19:58 +1000

    tested the models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,8 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :vehicle
-  validates :confirmation_status, inclusion: { in: ["Accept", "Regect"] }
-  validates :completion_status, inclusion: { in: ["Upcoming", "Active", "Completed"] }
+  validates :confirmation_status, inclusion: { in: ["accept", "reject"] }
+  validates :completion_status, inclusion: { in: ["upcoming", "active", "completed"] }
   validates :date_from, :date_to, presence: true
-  validates :description, length: { maximum: 500 }
+  validates :comment, length: { maximum: 500 }
 end
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -2,7 +2,7 @@ class Vehicle < ApplicationRecord
   belongs_to :user
   has_many :bookings
   validates :make, :model, :year, :number_of_passengers, :transmission, :price_per_day, presence: true
-  validates :transmission, inclusion: { in: ["Automatic", "Manual"] }
+  validates :transmission, inclusion: { in: ["automatic", "manual"] }
   validates :description, length: { maximum: 1000 }
   validates :year, numericality: true
 end
