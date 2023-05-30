40f42fbe6ab28964ee97ab7508dcc447abca5ef0
Jamie Heo <heojiyun@gmail.com>
2023-05-30 16:22:11 +1000

    vehicle new and create draft done

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -1,24 +1,28 @@
 class VehiclesController < ApplicationController
+  before_action :set_user, only: [:new, :create]
+
   def new
     @vehicle = Vehicle.new
   end
 
   def create
     @vehicle = Vehicle.new(vehicle_params)
+    @vehicle.user = current_user
     if @vehicle.save
-      redirect_to #path
+      redirect_to vehicle_path(@vehicle)
     else
-      render :new
+      render :new, status:
+      :unprocessable_entity
     end
   end
 
   private
 
   def set_user
-    @user = User.find(params[:user_id])
+    @user = current_user
   end
 
   def vehicle_params
-    params.require(:vehicle).permit(:make, :model, :year, :number_of_passengers, :transmission, :price_per_day, :user_id)
+    params.require(:vehicle).permit(:make, :model, :year, :number_of_passengers, :transmission, :price_per_day, :location, :description, :user_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  get 'new', to: "vehicles#new", as: :new_vehicle
-  post 'create', to: "vehicles#create"
+  post 'vehicles', to: "vehicles#create"
+  get 'vehicles/new', to: "vehicles#new"
 end
