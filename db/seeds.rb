b478ffe5ad9014f652366c907bec3af0f8d7d381
eshiyu park <eshiyupark@gmail.com>
2023-05-30 15:17:17 +1000

    Created draft seeds without testing. Added faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+Vehicle.delete_all
+puts "Deleted all vehicles"
+Booking.delete_all
+puts "Deleted all bookings"
+User.delete_all
+puts "Deleted all users"
+
+puts "Creating users..."
+
+user_one = User.new(email: "johndoe.@gmail.com", password: "secret", first_name: "John", last_name: "Doe", licence_number: "N12345", date_of_birth: "2023-05-30", phone_number: "91234567")
+user_one.save
+user_two = User.new(email: "janesmith.@gmail.com", password: "secret", first_name: "Jane", last_name: "Smith", licence_number: "X3245235", date_of_birth: "1999-02-22", phone_number: "+61422594506")
+user_two.save
+user_three = User.new(email: "monkeyman.@gmail.com", password: "secret", first_name: "Monkey", last_name: "Man", licence_number: "Y23145", date_of_birth: "1992-02-11", phone_number: "03-234-12321")
+user_three.save
+puts "Created users"
+
+puts "Creating 10 vehicles..."
+
+10.times do
+  vehicle = Vehicle.new(make: "#{Faker::Vehicle.make}", model: "#{Faker::Vehicle.model}", year: Faker::Vehicle.year, number_of_passengers: rand(4..7), transmission: ["automatic", "manual"].sample, price_per_day: rand(34.4..75.5).round(2), location: "#{Faker::Address.city}", description: "#{Faker::Quotes::Shakespeare.romeo_and_juliet_quote}")
+  vehicle.save
+end
+
+puts "Created vehicles"
