d28447319839ab0704c2002eae9381f3899c1ae5
Jamie Heo <heojiyun@gmail.com>
2023-05-30 15:29:51 +1000

    started vehicle new and creare user story

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -1,2 +1,24 @@
 class VehiclesController < ApplicationController
+  def new
+    @vehicle = Vehicle.new
+  end
+
+  def create
+    @vehicle = Vehicle.new(vehicle_params)
+    if @vehicle.save
+      redirect_to #path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def vehicle_params
+    params.require(:vehicle).permit(:make, :model, :year, :number_of_passengers, :transmission, :price_per_day, :user_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  get 'new', to: "vehicles#new", as: :new_vehicle
+  post 'create', to: "vehicles#create"
 end
