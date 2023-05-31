4a38ff9c530a4a1bf14016bc5ca8a9d28260083b
eshiyu park <eshiyupark@gmail.com>
2023-05-31 17:04:27 +1000

    Commented out all authorisation logic. Fixed spelling error in bookings_controller. Added links to access bookings#show. Unnested bookings#show route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  
+
   resources :vehicles do
-    resources :bookings, only: [:show]
+    resources :bookings, only: [:new, :create]
   end
 
   resources :my_bookings, only: [:index, :update]
-  resources :bookings, only: [:index]
+  resources :bookings, only: [:index, :show]
   resources :my_vehicles
 end
