529ab89c3556acd284c6ee9a1932af0d437f7e39
Ayanthi Saparamadu <nadeeshaddna@gmail.com>
2023-06-01 14:07:33 +1000

    from date, to date validated with back buttons

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -5,11 +5,12 @@ class Booking < ApplicationRecord
   # validates :completion_status, inclusion: { in: ["upcoming", "active", "completed"] }
   validates :date_from, :date_to, presence: true
   validates :comment, length: { maximum: 500 }
-  # validate :validate_end_date_before_start_date
 
-  # def validate_end_date_before_start_date
-  #   if date_to && date_from
-  #     errors.add(:date_to, "jdbsafkdb") if date_to < date_from
-  #   end
-  # end
+  validate :date_from_before_date_to
+
+  def date_from_before_date_to
+    if self.date_from > self.date_to
+      errors.add(:date_from, "should be before end date")
+    end
+  end
 end
