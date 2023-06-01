89473f7ed9bbf9601cc23bfd7faa7704552375cd
eshiyu park <eshiyupark@gmail.com>
2023-06-01 12:26:13 +1000

    Buttons for accept/deny now work correctly. Cancel button also appears

--- app/controllers/my_bookings_controller.rb
+++ app/controllers/my_bookings_controller.rb
@@ -6,7 +6,13 @@ class MyBookingsController < ApplicationController
   end
 
   def update
-
+    @my_booking = Booking.find(params[:id])
+    @my_booking.confirmation_status = params[:booking][:confirmation_status]
+    if @my_booking.save
+      redirect_to my_bookings_path
+    else
+      flash.alert = "There was an error"
+    end
   end
 
   private
--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -1,5 +1,6 @@
 class VehiclesController < ApplicationController
   before_action :set_user, only: [:new, :create, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: :index
 
   def index
     @vehicles = Vehicle.all
