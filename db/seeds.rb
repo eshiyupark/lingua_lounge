e5fe6ec2e92a0d25ef9c5adb739fe62c73e42eba
eshiyu park <eshiyupark@gmail.com>
2023-06-02 14:26:38 +1000

    Further styling. Edited seed file to support pitch

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,29 +15,39 @@ user_two = User.new(email: "janesmith@gmail.com", password: "secret", first_name
 user_two.save
 user_three = User.new(email: "monkeyman@gmail.com", password: "secret", first_name: "Monkey", last_name: "Man", licence_number: "Y23145", date_of_birth: "1992-02-11", phone_number: "03-234-12321")
 user_three.save
+
 puts "Created users"
 
-puts "Creating 15 vehicles..."
+puts "Creating 10 vehicles..."
 
-15.times do
+11.times do
   vehicle = Vehicle.new(make: "#{Faker::Vehicle.make}", model: "#{Faker::Vehicle.model}", year: Faker::Vehicle.year, number_of_passengers: rand(4..7), transmission: ["automatic", "manual"].sample, price_per_day: rand(34.4..75.5).round(2), location: "#{Faker::Address.full_address}", description: "#{Faker::Quotes::Shakespeare.romeo_and_juliet_quote}")
   vehicle.user = User.all.sample
   vehicle.save
 end
 
-puts "Created vehicles"
+puts "Created 10 vehicles"
 
-puts "Creating 2 vehicles with detailed location attributes, and owner as John Doe..."
-vehicle_one = Vehicle.new(make: "#{Faker::Vehicle.make}", model: "#{Faker::Vehicle.model}", year: Faker::Vehicle.year, number_of_passengers: rand(4..7), transmission: ["automatic", "manual"].sample, price_per_day: rand(34.4..75.5).round(2), location: "228 La Trobe Street, Melbourne, VIC 3000", description: "#{Faker::Quotes::Shakespeare.romeo_and_juliet_quote}")
+puts "Creating 4 vehicles with detailed location attributes, and owner as John Doe..."
+
+vehicle_one = Vehicle.new(make: "#{Faker::Vehicle.make}", model: "#{Faker::Vehicle.model}", year: Faker::Vehicle.year, number_of_passengers: rand(4..7), transmission: ["automatic", "manual"].sample, price_per_day: rand(34.4..102.5).round(2), location: "228 La Trobe Street, Melbourne, VIC 3000", description: "#{Faker::Quotes::Shakespeare.romeo_and_juliet_quote}")
 vehicle_one.user = user_one
 vehicle_one.save
-vehicle_two = Vehicle.new(make: "#{Faker::Vehicle.make}", model: "#{Faker::Vehicle.model}", year: Faker::Vehicle.year, number_of_passengers: rand(4..7), transmission: ["automatic", "manual"].sample, price_per_day: rand(34.4..75.5).round(2), location: "32 Beacon Hill Road, Beacon Hill, NSW 2100", description: "#{Faker::Quotes::Shakespeare.romeo_and_juliet_quote}")
+vehicle_two = Vehicle.new(make: "#{Faker::Vehicle.make}", model: "#{Faker::Vehicle.model}", year: Faker::Vehicle.year, number_of_passengers: rand(4..7), transmission: ["automatic", "manual"].sample, price_per_day: rand(34.4..75.5).round(2), location: "50 Queen Street, Melbourne, VIC 3000", description: "#{Faker::Quotes::Shakespeare.romeo_and_juliet_quote}")
 vehicle_two.user = user_one
 vehicle_two.save
+vehicle_three = Vehicle.new(make: "#{Faker::Vehicle.make}", model: "#{Faker::Vehicle.model}", year: Faker::Vehicle.year, number_of_passengers: rand(4..7), transmission: ["automatic", "manual"].sample, price_per_day: rand(34.4..75.5).round(2), location: "209 Swanston Street, Melbourne, VIC 3000", description: "#{Faker::Quotes::Shakespeare.romeo_and_juliet_quote}")
+vehicle_three.user = user_one
+vehicle_three.save
+vehicle_four = Vehicle.new(make: "#{Faker::Vehicle.make}", model: "#{Faker::Vehicle.model}", year: Faker::Vehicle.year, number_of_passengers: rand(4..7), transmission: ["automatic", "manual"].sample, price_per_day: rand(34.4..75.5).round(2), location: "66 Flinders Lane, Melbourne, VIC 3000", description: "#{Faker::Quotes::Shakespeare.romeo_and_juliet_quote}")
+vehicle_four.user = user_one
+vehicle_four.save
 
-puts "Created 2 vehicles with specific locations, and owner as John Doe"
+puts "Created 4 vehicles with specific locations, and owner as John Doe"
 
 puts "Creating fresh user with no vehicles and no bookings..."
+
 user_four = User.new(email: "jamieheo@gmail.com", password: "secret", first_name: "Jamie", last_name: "Heo", licence_number: "N12325", date_of_birth: "1994-03-22", phone_number: "91232567")
 user_four.save
+
 puts "Created user with no vehicles and bookings."
